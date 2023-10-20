d92e9667da521ab2503fc0c1e3c94877026e1527
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-10-20 20:25:32 +0600

    store dark them value to localstorage

--- src/Components/DarkTheme/DarkTheme.jsx
+++ src/Components/DarkTheme/DarkTheme.jsx
@@ -2,20 +2,76 @@ import { CiDark } from "react-icons/ci";
 import { BiSun } from "react-icons/bi";
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import { useEffect } from "react";
+
 
 const DarkTheme = () => {
     const {dark, setDark} = useContext(AuthContext);
+  
+  // const localData = JSON.parse(localStorage.getItem("darkTheme"));
+
+  // setDark(localData[0])
+
+  useEffect(()=>{
+       const localData = JSON.parse(localStorage.getItem("darkTheme"));
+       setDark(!localData[0])
+  },[])
+
+  // console.log(localData[0])
+  
+  const darkHandleClick = () => {
+        localStorage.clear()
+        const theme = [];
+
+        const localData = JSON.parse(localStorage.getItem("darkTheme"));
+
+        if (!localData) {
+          theme.push(dark);
+          localStorage.setItem("darkTheme", JSON.stringify(theme));
+        } else {
+          theme.push(...localData, dark);
+          localStorage.setItem("darkTheme", JSON.stringify(theme));
+        }
+
+
+        setDark(!theme[0])
+
+  }
+
+  const sunHandleClick = () =>{
+    localStorage.clear()
+    const theme = [];
+
+    const localData = JSON.parse(localStorage.getItem("darkTheme"));
+
+    if (!localData) {
+      theme.push(dark);
+      localStorage.setItem("darkTheme", JSON.stringify(theme));
+    } else {
+      theme.push(...localData, dark);
+      localStorage.setItem("darkTheme", JSON.stringify(theme));
+    }
+
+    setDark(!theme[0]);
+
+  }
+
+    
+    
   return (
     <div>
       {dark ? (
         <CiDark
           className="text-4xl font-black"
-          onClick={() => setDark(!dark)}
+          // onClick={() => setDark(!dark)}
+          onClick={darkHandleClick}
         ></CiDark>
       ) : (
-        <BiSun className="text-4xl" onClick={() => setDark(!dark)}></BiSun>
+        <BiSun className="text-4xl"
+        onClick={sunHandleClick}
+        //  onClick={() => setDark(!dark)}
+         ></BiSun>
       )}
-
     </div>
   );
 };
